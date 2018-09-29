8d79bc0d30ad8c60a4faefd19a4f91723e91c414
ahampal <amithampal@gmail.com>
2018-09-29 02:33:23 -0400

    added basic printcard testing

--- src/testAPI.c
+++ src/testAPI.c
@@ -2,9 +2,15 @@
 
 int main(int argc, char **argv) {
     Card *testCardOne = NULL;
+    char *printedCard = NULL;
     VCardErrorCode retVal;
     retVal = createCard(argv[argc - 1], &testCardOne);
     printf("result: %d\n", retVal);
+    printedCard = printCard(testCardOne);
+    if(printedCard != NULL) {
+        printf("%s\n", printedCard);
+        free(printedCard);
+    }
     deleteCard(testCardOne);
     return 0;
 }
