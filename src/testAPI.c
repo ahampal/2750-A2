41934e2453c5a799373096fe3b5fefbe507adb24
ahampal <amithampal@gmail.com>
2018-10-13 00:12:52 -0400

    added writecard test case

--- src/testAPI.c
+++ src/testAPI.c
@@ -1,34 +1,56 @@
-#include "VCardParser.h"
+#include "CardTestUtilities.h"
+
 #define CTEST 0
 #define PTEST 0
 #define WTEST 1
 
 int main(int argc, char **argv) {
-    Card *testCardOne;
+    Card *refCard;
+    Card *writtenCard;
     VCardErrorCode retVal;
     char *retString;
     char *printedCard = NULL;
 
     //createCard Test
-    retVal = createCard(argv[argc - 1], &testCardOne);
+    retVal = createCard(argv[argc - 1], &refCard);
     retString = printError(retVal);
     if(CTEST) printf("createCard Status: %s\n", retString);
     free(retString);
     retString = NULL;
 
     //printCard Test
-    printedCard = printCard(testCardOne);
+    printedCard = printCard(refCard);
     if(printedCard != NULL) {
         if(PTEST) printf("%s", printedCard);
         free(printedCard);
     }
 
     //writeCard Test
-    retVal = writeCard("writeOne.vcf", testCardOne);
+    retVal = writeCard("writeOne.vcf", refCard);
     retString = printError(retVal);
-    if(WTEST) printf("writeCard Status: %s\n", retString);
+    if(WTEST) {
+        if(retVal == OK) {
+            printf(GRN);
+        }
+        else {
+            printf(RED);
+        }
+        printf("writeCard Status: %s\n", retString);
+        printf(RESET);
+        retVal = createCard("writeOne.vcf", &writtenCard);
+        if(_tObjEqual(refCard,writtenCard)) {
+            printf(GRN);
+            printf("writeOne.vcf and %s are the same file\n", argv[argc-1]);
+        }
+        else {
+            printf(RED);
+            printf("writeOne.vcf and %s are not the same file\n", argv[argc-1]);
+        }
+        printf(RESET);
+        deleteCard(writtenCard);
+    }
     free(retString);
     retString = NULL;
-    deleteCard(testCardOne);
+    deleteCard(refCard);
     return 0;
 }
