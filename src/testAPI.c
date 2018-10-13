73ca26083be4dbe2df3db583ea2ee7b4c4a4f06e
ahampal <amithampal@gmail.com>
2018-10-12 22:45:49 -0400

    Bugfixes

--- src/testAPI.c
+++ src/testAPI.c
@@ -1,41 +1,34 @@
 #include "VCardParser.h"
-#define CTEST 1
-#define PTEST 0
+#define CTEST 0
+#define PTEST 1
 #define WTEST 1
 
 int main(int argc, char **argv) {
     Card *testCardOne;
     VCardErrorCode retVal;
     char *retString;
+    char *printedCard = NULL;
 
     //createCard Test
-    if(CTEST) {
-        retVal = createCard(argv[argc - 1], &testCardOne);
-        retString = printError(retVal);
-        printf("createCard Status: %s\n", retString);
-        free(retString);
-        retString = NULL;
-    }
+    retVal = createCard(argv[argc - 1], &testCardOne);
+    retString = printError(retVal);
+    if(CTEST) printf("createCard Status: %s\n", retString);
+    free(retString);
+    retString = NULL;
 
     //printCard Test
-    if(PTEST) {
-        char *printedCard = NULL;
-        printedCard = printCard(testCardOne);
-        if(printedCard != NULL) {
-            printf("%s\n", printedCard);
-            free(printedCard);
-            free(printedCard);
-        }
+    printedCard = printCard(testCardOne);
+    if(printedCard != NULL) {
+        if(PTEST) printf("%s", printedCard);
+        free(printedCard);
     }
 
     //writeCard Test
-    if(WTEST) {
-        retVal = writeCard("writeOne.vcf", testCardOne);
-        retString = printError(retVal);
-        printf("writeCard Status: %s\n", retString);
-        free(retString);
-        retString = NULL;
-        deleteCard(testCardOne);
-    }
+    retVal = writeCard("writeOne.vcf", testCardOne);
+    retString = printError(retVal);
+    if(WTEST) printf("writeCard Status: %s\n", retString);
+    free(retString);
+    retString = NULL;
+    deleteCard(testCardOne);
     return 0;
 }
